0728445806655cd76983970a45277da9bcf54a31
Synthetic <crashoveride001@gmail.com>
2018-11-13 21:03:28 +0200

    Temperature string -> float
    Change temperature var from string to float

--- RpiStats/Program.cs
+++ RpiStats/Program.cs
@@ -23,7 +23,7 @@ namespace RpiStats
 
             while (true)
             {
-                string temperature = GetTemperature();
+                var temperature = GetTemperature();
                 TemperatureOutput(temperature);
                 
                 
@@ -36,7 +36,7 @@ namespace RpiStats
             }
         }
 
-        private static void TemperatureOutput(string temperature)
+        private static void TemperatureOutput(float temperature)
         {
             TemperatureSetMinMax(temperature);
             ScreenOutput($"Temperature| Min: {_tempMin.ToString("#.0")} | Cur: {_tempAverage.ToString("#.0")} | Max: {_tempMax.ToString("#.0")}");
@@ -65,31 +65,22 @@ namespace RpiStats
             ScreenOutput(tempBar, true);
         }
 
-        private static void TemperatureSetMinMax(string temperature)
+        private static void TemperatureSetMinMax(float temperature)
         {
             try
             {
-                temperature = temperature.Trim();
-                double floatTemperature;
-                
-                if (double.TryParse(temperature, out floatTemperature))
-                {
-                    if (_tempAverage == 0.0)
-                        _tempAverage = floatTemperature;
-                    else
-                        _tempAverage = floatTemperature;
-                    //_tempAverage = (_tempAverage + floatTemperature) / 2;
+                if (_tempAverage == 0.0)
+                    _tempAverage = temperature;
+                else
+                    _tempAverage = temperature;
+                //_tempAverage = (_tempAverage + floatTemperature) / 2;
 
 
-                    if (_tempMin > floatTemperature)
-                        _tempMin = floatTemperature;
+                if (_tempMin > temperature)
+                    _tempMin = temperature;
 
-                    if (_tempMax < floatTemperature)
-                        _tempMax = floatTemperature;
-                } else
-                {
-                    ScreenOutput("temperature cannot be parsed : " + temperature);
-                }
+                if (_tempMax < temperature)
+                    _tempMax = temperature;
             }
             catch (Exception ex)
             {
@@ -98,7 +89,7 @@ namespace RpiStats
             }
         }
 
-        private static string GetTemperature()
+        private static float GetTemperature()
         {
             // bash command / opt / vc / bin / vcgencmd measure_temp
             var process = new Process()
@@ -119,8 +110,12 @@ namespace RpiStats
             // TESTING
             //var result = new Random().Next(30, 70).ToString() + ".3'C";
 
-            var temperature = result.Substring(result.IndexOf('=') + 1, result.IndexOf("'") - (result.IndexOf('=') + 1)).Replace('.', ',');
-            return temperature;
+            var temperatureResult = result.Substring(result.IndexOf('=') + 1, result.IndexOf("'") - (result.IndexOf('=') + 1)).Replace('.', ',');
+            var temperature = 0.0f;
+            if (float.TryParse(temperatureResult, out temperature))
+                return temperature;
+            else
+                return 0.0f;
         }
 
         private static void ScreenOutput(string outputLine = "", bool appendLine = false)
