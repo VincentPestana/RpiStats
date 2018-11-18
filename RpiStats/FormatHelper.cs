f9fdb435330935ef1cc6e44ac5110d807eedc6f8
Synthetic <crashoveride001@gmail.com>
2018-11-18 15:06:53 +0200

    Added FormatHelper
    Added FormatHelper to help with formatting out weird values to strings.

---
+++ RpiStats/FormatHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace RpiStats
+{
+    static class FormatHelper
+    {
+        internal static string FormatProcessAverages(string[] loadAverages)
+        {
+            var returnString = "";
+            foreach (var loadAverage in loadAverages)
+            {
+                returnString += loadAverage + "\t";
+            }
+            return returnString;
+        }
+    }
+}
