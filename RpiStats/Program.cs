beef2b9bc150018dfe244f6947c6496d917ac418
Synthetic <crashoveride001@gmail.com>
2018-11-18 15:07:31 +0200

    Better CPU load averages output
    Better CPU load averages output, now using tabs

--- RpiStats/Program.cs
+++ RpiStats/Program.cs
@@ -50,7 +50,8 @@ namespace RpiStats
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Raspberry Pi Temperature Monitor     Hit any key to stop");
             Console.WriteLine("Started : " + _startingDateTime.ToString() + " || " + DateTime.Now.ToString());
-            Console.WriteLine("CPU load: " + Monitoring.GetProcessAverage());
+            Console.WriteLine("5m\t 10m\t 15m CPU Load Averages");
+            Console.WriteLine(FormatHelper.FormatProcessAverages(Monitoring.GetProcessAverage()));
             Console.ResetColor();
             for (int i = 0; i < Console.BufferWidth; i++)
                 Console.Write('=');
